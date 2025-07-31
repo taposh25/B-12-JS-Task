3b919791ceb79767f9fb987528eceb6a616f5ef0
Taposh Barai <taposhbarai03@gmail.com>
2025-07-31 13:42:59 +0600

    array-task

---
+++ array.js
@@ -0,0 +1,62 @@
+/*                                    //Fisrt Task//
+
+let fruits = ['apple', 'orange', 'banana', 'coconat', 'watermilon'];
+// console.log(fruits.indexOf('coconat'));
+console.log(fruits[3]);
+fruits[1] = 'jambura';
+console.log(fruits);
+
+*/
+
+                                  //Second Task//
+
+/*
+
+let touristPlace = ['Jaflog', 'Shitakundo', 'Srimongol'];
+ touristPlace.push('kuwakata');
+//  console.log(touristPlace);
+touristPlace.push('Bandarban', 'Sent-martin');
+// console.log(touristPlace);
+touristPlace.pop();
+console.log(touristPlace);
+
+*/
+                      
+                           //Third Task//
+
+/*
+let books = ['poem', 'natok', 'chemistry', 'physics', 'biology', 'math'];
+// console.log(books.includes('poem'));
+console.log(books.indexOf('chemistry'));
+
+*/
+
+                             // Fourth Task //
+
+/*
+
+let names = ["Alice", "Bob", "Charlie"];  
+let age = 25;                             
+let isStudent = true;                    
+let person = { name: "John", age: 30 };  
+let hobbies = ["Reading", "Coding"];     
+let greeting = "Hello, world!";     
+
+
+console.log(Array.isArray(names));
+console.log(Array.isArray(greeting));
+console.log(Array.isArray(hobbies));
+console.log(Array.isArray(person));
+
+*/
+
+                      // Fifth-Task//
+
+let fruits = ["Apple", "Banana", "Mango"];
+let vegetables = ["Carrot", "Tomato", "Spinach"];
+
+let combinedArray = fruits.concat(vegetables);
+
+console.log("Fruits Array:", fruits);
+console.log("Vegetables Array:", vegetables);
+console.log("Combined Array:", combinedArray);
