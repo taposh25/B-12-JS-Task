1c9fa8db4345460124c9134dd07f9acd97731f41
Taposh Barai <taposhbarai03@gmail.com>
2025-08-06 12:52:19 +0600

    summary

---
+++ summary18.js
@@ -0,0 +1,18 @@
+/*
+const books = ['physics', 'css', 'chemistrty', 'javaScripy', 'math'];
+for (let i = 0; i < books.length; i++) {
+    console.log(books[i])
+    
+}
+// const reversed = books.reverse();
+const sortedArray = books.sort();
+console.log(sortedArray);
+*/
+
+const gadgest = [
+    {name: 'laptop', price: 35000, color: 'black', brand: 'lenevo'},
+    {name: 'watch', price: 3500, color: 'black', brand: 'samsung'},
+    {name: 'phone', price: 30000, color: 'white', brand: 'Apple'}
+]
+
+console.log(gadgest[0].name);
